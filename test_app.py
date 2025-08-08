e52fe5429f5ea2c3f61e40a29d53396b1193393b
DanceSerg <poskanovss@miicmi.ru>
2025-08-08 14:13:32 +0300

    Init commit

--- test_app.py
+++ test_app.py
@@ -1,13 +1,24 @@
-from app import app
 import pytest
+from app import app
 
 @pytest.fixture
 def client():
-    app.config["TESTING"] = True
+    app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
-def test_home_page(client):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert b"Hello, CI/CD World!" in response.data
+def test_hello_route(client):
+    rv = client.get('/')
+    assert b'Hello, Vercel with Flask!' in rv.data
+
+
+
+
+
+
+
+
+
+
+
+
